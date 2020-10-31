e551adc82d2fb4dd313ebdeff1d28f68c2e2fbc8
DavidMonteroPosadas <ing.montero.g.d@gmail.com>
2020-10-30 18:57:28 -0600

    Initial Commit

---
+++ src/app/members/photo-editor/photo-editor.component.ts
@@ -0,0 +1,37 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Photo } from 'src/app/_models/photo';
+import { FileUploader } from 'ng2-file-upload';
+import { environment } from 'src/environments/environment';
+import { AuthService } from 'src/app/_services/auth.service';
+@Component({
+  selector: 'app-photo-editor',
+  templateUrl: './photo-editor.component.html',
+  styleUrls: ['./photo-editor.component.scss'],
+})
+export class PhotoEditorComponent implements OnInit {
+  @Input() photos: Photo[];
+  uploader: FileUploader;
+  hasBaseDropZoneOver = false;
+  baseUrl = environment.apiUrl;
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.initializeUpoader();
+  }
+
+  public fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  initializeUpoader() {
+    this.uploader = new FileUploader({
+      url: this.baseUrl + 'users/' + this.authService.decodeToken.nameid + '/photos',
+      authToken: 'Bearer ' + localStorage.getItem('token'),
+      isHTML5: true,
+      allowedFileType: ['image'],
+      removeAfterUpload: true,
+      autoUpload: false,
+      maxFileSize: 10 * 1024 * 1024
+    });
+  }
+}
